f08ca438fb355125395b12522cde7d58c0b876d9
2458578637 <2458578637@qq.com>
2023-08-05 11:57:14 +0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path'
 import vercel from "solid-start-vercel"
 
 export default defineConfig({
   plugins: [
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      }
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   devOptions: {
+    //     enabled: true
+    //   }
+    // }),
     solid({
       inspect: false,
       ssr: true,
